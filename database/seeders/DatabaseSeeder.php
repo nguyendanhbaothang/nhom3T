ff7e477f8e1edfe9f56f80a8ace2afbd1d33c9f2
hoangxuanthang7622 <hoangxuanthang7622@gmail.com>
2022-12-14 17:26:55 +0700

    categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('categories')->insert([
+            [
+                'id' => 1,
+                'name' => 'dong ho deo tay',
+            ],
+            [
+                'id' => 2,
+                'name' => 'dong ho thong minh',
+            ]
+        ]);
     }
 }
