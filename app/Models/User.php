9e04ac4462de713429ad53bd664fe4d56f35d82e
tam21309 <belltam240297@gmail.com>
2022-12-15 21:22:05 +0700

    controller-user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,25 +3,30 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+// use App\Traits\HasPermissions;
+
+use App\Traits\HasPermissions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
+{   use Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,HasPermissions;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    // protected $fillable = [
+    //     'name',
+    //     'email',
+    //     'password',
+    // ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -41,4 +46,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function group()
+    {
+        return $this->belongsTo(Group::class, 'group_id', 'id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'user_id', 'id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\GroupController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,3 +34,33 @@ Route::group(['prefix' => 'products'], function () {
     Route::get('/trash', [ProductController::class, 'trash'])->name('product.trash');
     Route::put('/restoredelete/{id}', [ProductController::class, 'restoredelete'])->name('product.restoredelete');
 });
+// Route::resource('users',UserController::class);
+Route::group(['prefix' => 'groups'], function () {
+    Route::get('/', [GroupController::class, 'index'])->name('group.index');
+    Route::get('/create', [GroupController::class, 'create'])->name('group.create');
+    Route::post('/store', [GroupController::class, 'store'])->name('group.store');
+    Route::get('/edit/{id}', [GroupController::class, 'edit'])->name('group.edit');
+    Route::put('/update/{id}', [GroupController::class, 'update'])->name('group.update');
+    Route::delete('destroy/{id}', [GroupController::class, 'destroy'])->name('group.destroy');
+    // trao quyền
+    Route::get('/detail/{id}', [GroupController::class, 'detail'])->name('group.detail');
+    Route::put('/group_detail/{id}', [GroupController::class, 'group_detail'])->name('group.group_detail');
+});
+
+Route::get('/hi', function () {
+    return view('admin.group.index');
+});
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index'])->name('user.index');
+    Route::get('/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/store', [UserController::class, 'store'])->name('user.store');
+    Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
+    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/update/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::get('/editpass/{id}', [UserController::class, 'editpass'])->name('user.editpass');
+    Route::put('/updatepass/{id}', [UserController::class, 'updatepass'])->name('user.updatepass');
+    Route::get('/adminpass/{id}', [UserController::class, 'adminpass'])->name('user.adminpass');
+    Route::put('/adminUpdatePass/{id}', [UserController::class, 'adminUpdatePass'])->name('user.adminUpdatePass');
+   });
